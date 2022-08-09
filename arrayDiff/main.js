455f03123d3bb19ff6695f622cebe34ac723c3cd
SinaLiri <Sina.Liri@yahoo.com>
2022-08-09 12:14:42 +0430

    challenge 2 complete

--- arrayDiff/main.js
+++ arrayDiff/main.js
@@ -1,14 +1,22 @@
-let result=[]
+
 
 const arrayDiff = (arr1, arr2) => {
+    let result=[]
     arr1.map(item =>{
         if(arr2.indexOf(item)=== -1){
             result.push(item)
         }
 
     })
+    // arr2.map(item =>{
+    //     if(arr1.indexOf(item)=== -1){
+    //         result.push(item)
+    //     }
+
+    // })
 
     return result;
 
 };
 console.log(arrayDiff([1,2,3],[1,2]));
+// console.log(arrayDiff([1,2,3],[4,5,6]));
